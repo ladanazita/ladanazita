63a2132c0bc0eacbe0dbbd16dd3a5674d8c6a072
Ladan Nasserian <ladannasserian@gmail.com>
2015-06-16 17:59:34 -0700

    corrected icons

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,7 +1,13 @@
 'user strict';
 
 angular
-  .module('app', ['ngRoute'])
+  .module('app', ['ngRoute', 'ngResource'])
+  .factory('Work', getData)
   .config(['$routeProvider', function($routeProvider){
     $routeProvider.otherwise({redirectTo: '/'})
   }]);
+
+  function getData($resource){
+    return $resource('data.json', {},{
+      query: {method: 'GET', isArray:true}
+    });
